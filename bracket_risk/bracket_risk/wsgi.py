8b3995ac177fac311d29e98349c03ad59a2ef589
STatsuno <statsuno@russellinvestments.com>
2021-02-14 19:36:03 -0800

    first commit

---
+++ bracket_risk/bracket_risk/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for bracket_risk project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bracket_risk.settings')
+
+application = get_wsgi_application()
